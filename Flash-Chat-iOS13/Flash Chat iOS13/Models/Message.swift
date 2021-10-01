e0265fd5f52a444614d9fbac6b4581bbafee56a2
Henrique <henriquelegnaro@gmail.com>
2021-10-01 12:52:34 -0300

    Chat App

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Virtual Machine on 30/09/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
